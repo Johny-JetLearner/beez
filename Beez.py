5357d8b0e14fb6ea9d9c5eb98460b84afe448a59
Johny-JetLearner <giannistempelis00@gmail.com>
2026-02-23 20:00:57 +0200

    Add files via upload

--- Beez.py
+++ Beez.py
@@ -5,6 +5,7 @@ HEIGHT=500
 bee=Actor("bee")
 bee.x=300
 bee.y=200
+score=0
 flower=Actor("flower")
 flower.x=random.randint(0,500)
 flower.y=random.randint(0,500)
@@ -12,4 +13,19 @@ def draw():
     screen.blit("grass",(0,0))
     bee.draw()
     flower.draw()
+    screen.draw.text(str(score),center=(50,50))
+def update():
+    global score 
+    if keyboard.up:
+        bee.y=bee.y-5
+    if keyboard.down:
+        bee.y=bee.y+5
+    if keyboard.right:
+        bee.x=bee.x+5
+    if keyboard.left:
+        bee.x=bee.x-5
+    if bee.colliderect(flower):
+        flower.x=random.randint(0,500)
+        flower.y=random.randint(0,500)
+        score=score+10
 pgzrun.go()
